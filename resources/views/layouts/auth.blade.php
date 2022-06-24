9c6c194e1851957e5f779083624ba4da40dfbd33
mahimtalukder <19-41621-3@student.aiub.edu>
2022-06-24 22:32:37 +0600

    Login change

--- resources/views/layouts/auth.blade.php
+++ resources/views/layouts/auth.blade.php
@@ -20,22 +20,22 @@
     <!-- End fonts -->
 
     <!-- core:css -->
-    <link rel="stylesheet" href="../../../assets/vendors/core/core.css">
+    <link rel="stylesheet" href="../assets_2/vendors/core/core.css">
     <!-- endinject -->
 
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
 
     <!-- inject:css -->
-    <link rel="stylesheet" href="../../../assets/fonts/feather-font/css/iconfont.css">
-    <link rel="stylesheet" href="../../../assets/vendors/flag-icon-css/css/flag-icon.min.css">
+    <link rel="stylesheet" href="../assets_2/fonts/feather-font/css/iconfont.css">
+    <link rel="stylesheet" href="../assets_2/vendors/flag-icon-css/css/flag-icon.min.css">
     <!-- endinject -->
 
     <!-- Layout styles -->
-    <link rel="stylesheet" href="../../../assets/css/demo1/style.min.css">
+    <link rel="stylesheet" href="../assets_2/css/demo1/style.min.css">
     <!-- End layout styles -->
 
-    <link rel="shortcut icon" href="../../../assets/images/favicon.png" />
+    <link rel="shortcut icon" href="../assets_2/images/favicon.png" />
 </head>
 
 <body>
@@ -59,11 +59,11 @@
     </div>
 
     <!-- core:js -->
-    <script src="../../../assets/vendors/core/core.js"></script>
+    <script src="../assets_2/vendors/core/core.js"></script>
     <!-- endinject -->
 
     <!-- inject:js -->
-    <script src="../../../assets/vendors/feather-icons/feather.min.js"></script>
+    <script src="../assets_2/vendors/feather-icons/feather.min.js"></script>
     <!-- endinject -->
 
 </body>
