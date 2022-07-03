18fb70f7c32f7d5ed8e7b825a425e20842a01759
mahimtalukder <19-41621-3@student.aiub.edu>
2022-07-04 04:56:19 +0600

    feature update

--- database/seeders/NoticeSeeder.php
+++ database/seeders/NoticeSeeder.php
@@ -16,7 +16,8 @@ class NoticeSeeder extends Seeder
     {
         DB::table('notices')->insert([
             'title' => 'Re-instating COVID-19 Safety Protocols',
-            'notice' => 'Every student, faculty member and official, while entering AIUB campus must wear mask at all times and must be screened through the temperature checking tunnel. Anyone having body temperature more than 100 degree Fahrenheit (37.7 degree Celsius) must not enter the premise and return to home immediately.'
+            'notice' => 'Every student, faculty member and official, while entering AIUB campus must wear mask at all times and must be screened through the temperature checking tunnel. Anyone having body temperature more than 100 degree Fahrenheit (37.7 degree Celsius) must not enter the premise and return to home immediately.',
+            'club_id' => 2
         ]);
     }
 }
