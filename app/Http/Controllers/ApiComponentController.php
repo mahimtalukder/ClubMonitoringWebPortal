6e603294521a84f1dcfda47d03477372c2c3e3a4
sayedsyfuzzaman <sayedsyfuzzaman@gmail.com>
2022-08-10 01:45:34 +0600

    Component Api

--- app/Http/Controllers/ApiComponentController.php
+++ app/Http/Controllers/ApiComponentController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Component;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Session;
+use function PHPUnit\Framework\isEmpty;
 
 class ApiComponentController extends Controller
 {
@@ -12,5 +13,32 @@ class ApiComponentController extends Controller
 
         $components = Component::all();
         return $components;
+    }
+
+    public function addComponents(Request $request){
+        $validate = $request->validate([
+            "name" => "required",
+            "description" => "required"
+        ]);
+
+
+        $components = new Component();
+        $components->name = $request->name;
+        $components->description = $request->description;
+        $components->added_by = $request->user_id;
+        $components->save();
+
+        return "success";
+
+    }
+    public function IsComponentNameExist(Request $request){
+        $isExist = Component::where("name", $request->name)
+            ->distinct()->first();
+
+        if($isExist){
+            return "true";
+        }else{
+            return "false";
+        }
     }
 }
